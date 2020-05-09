7391769ef869810f0f0922c6b1460b8a0bbce1cb
overmaind <overelectric@gmail.com>
2020-05-09 20:22:18 +0900

    Ratatouille init

---
+++ Application/src/main/java/myapp/MyAppController.java
@@ -0,0 +1,20 @@
+package myapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MyAppController {
+    
+    @RequestMapping("/")
+    public String index() {
+        return "index.html";
+    }
+
+    @RequestMapping("/board")
+    public String board() {
+        return "board.html";
+    }
+}
