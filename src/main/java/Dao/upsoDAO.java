2f1ea1e2949e24136f0996bfd8fac10969783eaa
Snail <soykim76@gmail.com>
2020-05-26 15:50:52 +0900

    wordcloud 86개 (잠실)

--- src/main/java/Dao/upsoDAO.java
+++ src/main/java/Dao/upsoDAO.java
@@ -13,7 +13,7 @@ import Vo.SearchVO;
 import java.io.BufferedReader;
 
 import java.io.InputStreamReader;
-
+import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.util.ArrayList;
